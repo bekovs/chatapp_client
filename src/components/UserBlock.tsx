0ef49f686acb5335815827801bf2c7ae64a5b9bd
Sultan <bekovs96@gmail.com>
2023-10-27 14:36:35 +0600

    added styles for user block

--- src/components/UserBlock.tsx
+++ src/components/UserBlock.tsx
@@ -1,12 +1,14 @@
 function UserBlock() {
   return (
     <div className="user_box">
+      <div>
         <h3>Hello, guest</h3>
         <form>
             <input type="text" />
             <button>save</button>
         </form>
-        <p className="copyright">ChatApp © 2023</p>
+      </div>
+      <p className="copyright">ChatApp © 2023</p>
     </div>
   )
 }
